84eb78dfab7f630720676b8afaf028b6dc4449cf
Arturno <37472216+Arturno@users.noreply.github.com>
2019-05-07 07:17:45 +0200

    Update PID.cpp

--- program2/TX/src/PID.cpp
+++ program2/TX/src/PID.cpp
@@ -1,87 +1,36 @@
-#include "../headers/PID.hpp"
+#ifndef PID
+#define PID
+
+#include <iostream>
+#include <cmath>
+#include "PID.hpp"
 
 using namespace std;
 
-class PIDImpl
-{
-    public:
-        PIDImpl( double dt, double max, double min, double Kp, double Kd, double Ki );
-        ~PIDImpl();
-        double calculate( double setpoint, double pv );
-
-    private:
-        double _dt;
-        double _max;
-        double _min;
-        double _Kp;
-        double _Kd;
-        double _Ki;
-        double _pre_error;
-        double _integral;
-};
-
-
-PID::PID( double dt, double max, double min, double Kp, double Kd, double Ki )
-{
-    pimpl = new PIDImpl(dt,max,min,Kp,Kd,Ki);
-}
-double PID::calculate( double setpoint, double pv )
-{
-    return pimpl->calculate(setpoint,pv);
-}
-PID::~PID() 
-{
-    delete pimpl;
-}
-
-
-/**
- * Implementation
- */
-PIDImpl::PIDImpl( double dt, double max, double min, double Kp, double Kd, double Ki ) :
+PID::PID(double dt, double max, double min, double Kp, double Kd, double Ki):
     _dt(dt),
     _max(max),
     _min(min),
     _Kp(Kp),
     _Kd(Kd),
     _Ki(Ki),
-    _pre_error(0),
-    _integral(0)
+    previous_error(0),
+    integral(0)
 {
 }
 
-double PIDImpl::calculate( double setpoint, double pv )
+double PID:calculate(double setpoint, double measured_value)
 {
-    
-    // Calculate error
-    double error = setpoint - pv;
-
-    // Proportional term
-    double Pout = _Kp * error;
-
-    // Integral term
-    _integral += error * _dt;
-    double Iout = _Ki * _integral;
-
-    // Derivative term
-    double derivative = (error - _pre_error) / _dt;
-    double Dout = _Kd * derivative;
-
-    // Calculate total output
-    double output = Pout + Iout + Dout;
-
-    // Restrict to max/min
-    if( output > _max )
+	double error = setpoint - measured_value;
+	integral += error * _dt;
+	double derivative = (error - previous_error) / _dt;
+	double output  = _Kp * error + _Ki * integral + _Kd * derivative;
+	previous_error = error;
+	
+	if( output > _max )
         output = _max;
     else if( output < _min )
         output = _min;
-
-    // Save error to previous error
-    _pre_error = error;
-
-    return output;
+	return output
 }
-
-PIDImpl::~PIDImpl()
-{
-}
+#endif
